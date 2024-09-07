0915433a86e97ae51d39ad1e6ec5d89d1b22e51f
Felix <93457386+feliquisds@users.noreply.github.com>
2024-09-07 15:48:43 -0300

    feat(class); added RA class

---
+++ FazerVsCode/src/RA2920482411020.java
@@ -0,0 +1,87 @@
+public class RA2920482411020 {
+    private String name;
+    private String ra;
+    private int age;
+    private String githubLink;
+    private String linkedinLink;
+    private String course;
+    private int semester;
+
+    private RA2920482411020() {
+        this.name = "Richard Felix de Souza";
+        this.ra = "2920482411020";
+        this.age = 18;
+        this.course = "Análise e Desenvolvimento de Sistemas";
+        this.semester = 1;
+    }
+
+
+
+    // name methods
+    public String getName() {
+        return name;
+    }
+    public void setName(String _name) {
+        this.name = _name;
+    }
+
+
+
+    // RA methods
+    public String getRA() {
+        return ra;
+    }
+
+
+
+    // age methods
+    public int getIdade() {
+        return age;
+    }
+    public void setIdade(int _age) {
+        this.age = _age;
+    }
+
+
+
+    // github methods
+    public String getGithub() {
+        return githubLink;
+    }
+    public void setGithub(String _githubLink) {
+        this.githubLink = _githubLink;
+    }
+    public void removeGithub() {
+        this.githubLink = null;
+    }
+
+
+
+    // linkedin methods
+    public String getLinkedin() {
+        return linkedinLink;
+    }
+    public void setLinkedin(String _linkedinLink) {
+        this.linkedinLink = _linkedinLink;
+    }
+    public void removeLinkedin() {
+        this.linkedinLink = null;
+    }
+
+
+
+    // course methods
+    public String getCourse() {
+        return course;
+    }
+
+
+
+    // semester methods
+    public int getSemester() {
+        return semester;
+    }
+    public void setSemester(int _semester) {
+        this.semester = _semester;
+    }
+}
